8b8fc3fc5623cdbe57104442ff5d0377f69acb9f
Julius Lipp <lipp.julius@gmail.com>
2024-01-05 12:15:49 +0100

    first commit

---
+++ dist/models/index.d.ts
@@ -0,0 +1,5 @@
+export * from './Embeddings200Response';
+export * from './Embeddings200ResponseDataInner';
+export * from './Embeddings200ResponseUsage';
+export * from './EmbeddingsRequest';
+export * from './ErrorResponse';
---
+++ src/models/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Embeddings200Response';
+export * from './Embeddings200ResponseDataInner';
+export * from './Embeddings200ResponseUsage';
+export * from './EmbeddingsRequest';
+export * from './ErrorResponse';
