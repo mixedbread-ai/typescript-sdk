caad5facc25458053ebaf1175ee186a1f0e02115
Julius Lipp <lipp.julius@gmail.com>
2024-01-28 11:19:51 +0100

    bug: unprocessable entity error msg

--- src/MixedbreadAi.ts
+++ src/MixedbreadAi.ts
@@ -59,6 +59,7 @@ export class MixedbreadAi {
                 401: `Unauthorized. Authentication failed, or the user lacks the necessary permissions for the requested operation.`,
                 402: `Required Payment. The request is valid but cannot proceed without a balance top-up.`,
                 404: `Not Found. The requested resource is not available or does not exist.`,
+                422: `Unprocessable Entity. The request was well-formed but was unable to be followed due to semantic errors.`,
                 429: `Too Many Requests. Too many requests have been sent in a given amount of time, exceeding the rate limit.`,
                 500: `Internal Server Error. An unexpected error occurred on the server side.`,
             },
