0d8a705539da77c1a3dd376c347fa1b59d0bfb40
Julius Lipp <lipp.julius@gmail.com>
2024-07-09 17:54:05 +0200

    Fix stream

--- src/Client.ts
+++ src/Client.ts
@@ -16,11 +16,8 @@ export declare namespace MixedbreadAIClient {
     }
 
     interface RequestOptions {
-        /** The maximum time to wait for a response in seconds. */
         timeoutInSeconds?: number;
-        /** The number of times to retry the request. Defaults to 2. */
         maxRetries?: number;
-        /** A hook to abort the request. */
         abortSignal?: AbortSignal;
     }
 }
